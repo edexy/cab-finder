889196a08ff6c1aec295b6b6e8a081e0602fa7fb
Peter Edoja <edoja2012@gmail.com>
2021-08-03 08:43:10 +0100

    allow cors

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.use(
     swaggerUi.setup(swaggerDocument)
 );
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 app.use('/api/v1/driver', usersRouter);
 app.use('/api/v1/cars', carsRouter);
 
